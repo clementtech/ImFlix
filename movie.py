081563e227025b6e2b5dc5ad70a184289b49dd14
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-02-16 12:18:28 +0800

    push movie.py
    initial draft

---
+++ movie.py
@@ -0,0 +1,36 @@
+from imdb import Cinemagoer
+import webbrowser
+
+software_option = str(input("ID/Watch: ")).lower()
+
+if software_option == "id":
+
+    movie = Cinemagoer()
+
+    search = str(input("Movie Name: "))
+
+    movie_search = movie.search_movie(search)
+
+    print(movie_search)
+
+elif software_option == "watch":
+
+    movie_type = str(input("Movie or Series? ")).lower()
+
+    if movie_type == "movie":
+
+        id = str(input("IMDB Movie ID: "))
+
+        vidsrc_api = f"https://vidsrc.dev/embed/movie/tt{id}"
+
+        webbrowser.open_new_tab(vidsrc_api)
+
+    elif movie_type == "series":
+
+        id = str(input("Movie IMDB id: "))
+        season = str(input("Movie Season? "))
+        episode = str(input("Movie Episode? "))
+
+        vidsrc_api = f"https://vidbinge.dev/embed/tv/tt{id}/{season}/{episode}"
+
+        webbrowser.open_new_tab(vidsrc_api)
