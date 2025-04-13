c22c212b3b564993336778cb636f75a705b7b0f1
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-04-13 05:32:29 +0000

    Add comment and improve code readability

--- movie.py
+++ movie.py
@@ -1,22 +1,33 @@
+# Description: This script searches for a movie using the IMDb API and opens its streaming link in a web browser.
+# Please install the required libraries before running the script.
+# pip install Cinemagoer
 from imdb import Cinemagoer
 import webbrowser
-import re
+import sys
 
+# Call the Cinemagoer API
 movie = Cinemagoer()
 
+# Prompt the user for a movie name
+print("Please enter the movie name you want to search for:")
 search = str(input("Movie Name: "))
 
+# If the user does not enter a movie name, exit the script
+if not search:
+    sys.exit("No movie name entered. Exiting.")
+
+# Search for the movie using the Cinemagoer API
 movie_search = movie.search_movie(search)
 
+# Select the first result from the search
 movai = str(movie_search).split(", ")[0]
 
-regex = r"^<Movie id:.+[http] title:_.+ (None)_>$"
+# Extract the movie ID from the search result
+id = (movai.split("[<Movie id:")[1].split("[http]")[0])
 
-if regex:
-    test = (movai.split("[<Movie id:"))
-
-id = (test[1].split("[http]")[0])
-
+# Construct the URL for the streaming link
+# Streaming service is provided by vidsrc API
 vidsrc_api = f"https://vidsrc.to/embed/movie/tt{id}"
 
+# Open the streaming link in a new web browser tab
 webbrowser.open_new_tab(vidsrc_api)
