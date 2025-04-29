8da32612cd2cc27bcedb2c753a18f130ab8e5b43
clementtech <119944151+clementtech@users.noreply.github.com>
2025-04-29 13:17:16 +0800

    Update movie.py
    Add try and except block to handle errors

--- movie.py
+++ movie.py
@@ -4,12 +4,17 @@ import sys
 import re
 
 movie = Cinemagoer()
+try:
+    print("Please enter the movie name you want to search for:")
+    search = input("Movie Name: ")
 
-print("Please enter the movie name you want to search for:")
-search = input("Movie Name: ")
+    id = (movie.search_movie(search))[0].movieID
 
-id = (movie.search_movie(search))[0].movieID
 
-vidsrc_api = f"https://vidsrc.to/embed/movie/tt{id}"
 
-webbrowser.open_new_tab(vidsrc_api)
+    vidsrc_api = f"https://vidsrc.to/embed/movie/tt{id}"
+
+    # webbrowser.open_new_tab(vidsrc_api)
+
+except IndexError:
+    sys.exit("Please enter a movie name.")
